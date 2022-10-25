9f3c9b36063d800aec2169e2a422a67aed6a8d98
IbrokhimMirzaev <mirzaevibrokhim@gmail.com>
2022-10-25 19:18:39 +0500

    chat body part added

--- lib/ui/chat/chat.dart
+++ lib/ui/chat/chat.dart
@@ -1,9 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'package:task_project/data/cubits/auth_cubit/auth_cubit.dart';
 import 'package:task_project/data/cubits/chat_cubit/chat_cubit.dart';
 import 'package:task_project/data/models/message_item.dart';
+import 'package:task_project/ui/chat/widgets/left_side_message_item.dart';
+import 'package:task_project/ui/chat/widgets/rightside_message_item.dart';
+import 'package:task_project/utils/colors.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({Key? key}) : super(key: key);
@@ -30,7 +34,80 @@ class _ChatPageState extends State<ChatPage> {
           )
         ],
       ),
-      // body:
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder<List<MessageItem>>(
+              stream: context.read<ChatCubit>().getMessages(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Center(child: Text(snapshot.error.toString()));
+                } else if (snapshot.hasData) {
+                  final messages = snapshot.data!;
+                  return messages.isNotEmpty
+                      ? ListView(
+                          physics: const BouncingScrollPhysics(),
+                          children: List.generate(messages.length, (index) {
+                            var message = messages[index];
+                            return (message.uid == user!.uid)
+                                ? RightSideMessageItem(
+                                    dateText: DateFormat.Hm().format(message.createdAt),
+                                    messageText: message.message,
+                                  )
+                                : LeftSideMessageItem(
+                                    dateText: DateFormat.Hm().format(message.createdAt),
+                                    messageText: message.message,
+                                  );
+                          }),
+                        )
+                      : const Center(child: Text("List Empty"));
+                }
+                return const Center(child: CircularProgressIndicator());
+              },
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: TextField(
+                    controller: controller,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          width: 2,
+                          color: MyColors.C_0001FC,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () async {
+                  if (controller.text.isNotEmpty) {
+                    await context.read<ChatCubit>().sendMessage(
+                          messageItem: MessageItem(
+                            uid: user!.uid,
+                            message: controller.text,
+                            createdAt: DateTime.now(),
+                            messageId: "",
+                            senderName: user.email!,
+                          ),
+                        );
+
+                    controller.clear();
+                  }
+                },
+                icon: const Icon(Icons.send, color: Colors.blue),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
