2731ec68dc034e1e499f2784517af6137440d3de
IbrokhimMirzaev <mirzaevibrokhim@gmail.com>
2022-10-27 19:13:30 +0500

    devices page added

---
+++ lib/ui/devices/devices_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DevicesPage extends StatefulWidget {
+  const DevicesPage({Key? key}) : super(key: key);
+
+  @override
+  State<DevicesPage> createState() => _DevicesPageState();
+}
+
+class _DevicesPageState extends State<DevicesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
